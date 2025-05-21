16bdfb62352c7f11a5b31ff00889cf2e76a69e79
Mateus Emanuel <120513275+mateus1450@users.noreply.github.com>
2025-05-21 17:49:59 -0300

    Add files via upload
    Projete um programa que use um loop while e solicite continuamente que o usuário insira uma palavra, a menos que o usuário insira "chupacabra" como a palavra de saída secreta, caso em que a mensagem "Você saiu do loop com sucesso". Deve ser impresso na tela, e o loop deve terminar.

---
+++ atv_comando_break.py
@@ -0,0 +1,12 @@
+palavra= str
+
+while palavra != "chupacabra":
+    palavra= input ("digite uma palavra: ")
+    if palavra == "chupacabra":
+        print("Saiu do loop com sucesso")
+        break
+        
+    
+    
+  
+    
